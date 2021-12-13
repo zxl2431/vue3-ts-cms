501845fd2e619f02f73bccbc55a912e5f0b7ffd9
zhangxianlin <zhangxianlin2431@163.com>
2021-12-14 01:08:01 +0800

    修改App.vue中的显示测试

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App test" />
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App OK" />
 </template>
 
 <script lang="ts">
