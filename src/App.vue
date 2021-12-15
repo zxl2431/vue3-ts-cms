3edb42198ce2c3e5a0f981393f02ec79928d5f47
zhangxianlin <zhangxianlin2431@163.com>
2021-12-16 00:09:21 +0800

    安装vuerouter,并创建views两个页面和routers进行简单的测试

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App OK" />
+  <div class="app">
+    <router-link to="/main">主页</router-link>
+    <router-link to="/login">登录</router-link>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 })
 </script>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.app {
+  height: 100%;
 }
 </style>
