3f6b508a525e511f2a1c774b6272d262a9477f3a
zhangxianlin <zhangxianlin2431@163.com>
2021-12-17 02:25:34 +0800

    增加vuex模块

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="app">
+    <div>{{ $store.state.name }}</div>
     <router-link to="/main">主页</router-link>
     <router-link to="/login">登录</router-link>
     <router-view></router-view>
---
+++ src/store/index.ts
@@ -0,0 +1,11 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+  state() {
+    return {
+      name: 'Mr.zhang'
+    }
+  }
+})
+
+export default store
