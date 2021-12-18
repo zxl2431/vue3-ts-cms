fce4ab0b74b4508ac309e15a761b6160162405c4
zhangxianlin <zhangxianlin2431@163.com>
2021-12-18 10:18:15 +0800

    全局引入element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="app">
+    <el-row>
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+      <el-button>中文</el-button>
+    </el-row>
     <div>{{ $store.state.name }}</div>
     <router-link to="/main">主页</router-link>
     <router-link to="/login">登录</router-link>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 import router from './router'
 import store from './store'
 
 const app = createApp(App)
 app.use(router)
 app.use(store)
+app.use(ElementPlus)
 app.mount('#app')
 // console.log(process.env.NODE_ENV)
