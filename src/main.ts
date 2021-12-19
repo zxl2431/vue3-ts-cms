6e944bb8620db0bf665148c937c6402052d3d0e2
zhangxianlin <zhangxianlin2431@163.com>
2021-12-19 16:37:44 +0800

    用户登录功能login->login-panel->login-account

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import App from './App.vue'
 
 import './assets/css/index.less'
 
+// 全局引入element-plus组件
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+// 全局引入element-plus/icons
+import * as Icons from '@element-plus/icons-vue'
 
 import router from './router'
 import store from './store'
@@ -16,6 +19,10 @@ const app = createApp(App)
 app.use(router)
 app.use(store)
 app.use(ElementPlus)
+// 注册Icons 全局组件
+for (const name in Icons) {
+  app.component(name, (Icons as any)[name])
+}
 app.mount('#app')
 // console.log(process.env.NODE_ENV)
 
---
+++ src/views/login/cpns/login-panel.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="login-panel">
+    <h1 class="title">后台管理系统</h1>
+    <el-tabs type="border-card" stretch>
+      <el-tab-pane>
+        <template #label>
+          <span>
+            <el-icon><circle-check-filled /></el-icon>账号登录</span
+          >
+        </template>
+        <login-account></login-account>
+      </el-tab-pane>
+      <el-tab-pane>
+        <template #label>
+          <span>
+            <el-icon><iphone /></el-icon>
+            手机登录
+          </span>
+        </template>
+        手机登录
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import LoginAccount from './login-account.vue'
+
+export default defineComponent({
+  components: {
+    LoginAccount
+  },
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>
