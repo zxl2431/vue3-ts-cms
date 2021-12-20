e8b72e52fcb67e010309c8c0c63adc609cd2ddf4
zhangxianlin <zhangxianlin2431@163.com>
2021-12-20 23:22:42 +0800

    登录成功之后跳转到views/main/main.vue,完成main.vue的容器布局aside,header,main

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app">
-    <router-link to="/main">主页</router-link>
-    <router-link to="/login">登录</router-link>
+    <!-- <router-link to="/main">主页</router-link>
+    <router-link to="/login">登录</router-link> -->
     <router-view></router-view>
   </div>
 </template>
