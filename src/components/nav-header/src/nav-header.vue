9d979bb5210df3232d606f6b9d458df7a44faffd
zhangxianlin <zhangxianlin2431@163.com>
2021-12-22 03:49:21 +0800

    开始写nav-header加一个图标控制左侧导航栏的展开和收起

---
+++ src/components/nav-header/src/nav-header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="nav-header">
+    <component
+      class="fold-meun"
+      :is="iconName"
+      @click="handleFoldClick"
+    ></component>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  emits: ['foldChange'],
+
+  setup(props, { emit }) {
+    const iconName = ref('Fold')
+
+    const isFold = ref(false)
+
+    const handleFoldClick = () => {
+      isFold.value = !isFold.value
+      iconName.value = isFold.value ? 'Expand' : 'Fold'
+      emit('foldChange', isFold.value)
+    }
+
+    return {
+      iconName,
+      handleFoldClick
+    }
+  }
+})
+</script>
+
+<style scoped lang="less">
+.nav-header {
+  .fold-meun {
+    width: 30px;
+    height: 30px;
+    cursor: pointer;
+  }
+}
+</style>
