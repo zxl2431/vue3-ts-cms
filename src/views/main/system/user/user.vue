65e351d0667814dc18e429aa641357d8973a26db
zhangxianlin <zhangxianlin2431@163.com>
2021-12-25 18:39:30 +0800

    开始写user页面,初步搭建user界面的search部分

--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -1,5 +1,42 @@
 <template>
-  <div>USER</div>
+  <div class="user">
+    <el-form label-width="120px">
+      <el-row>
+        <el-col span="8">
+          <el-form-item label="id">
+            <el-input placeholder="请输入id" />
+          </el-form-item>
+        </el-col>
+        <el-col span="8">
+          <el-form-item label="用户名">
+            <el-input placeholder="请输入用户名" />
+          </el-form-item>
+        </el-col>
+        <el-col span="8">
+          <el-form-item label="密码">
+            <el-input placeholder="请输入密码" />
+          </el-form-item>
+        </el-col>
+        <el-col span="8">
+          <el-form-item label="喜欢的运动">
+            <el-select placeholder="请选择喜欢的运动">
+              <el-option>篮球</el-option>
+              <el-option>足球</el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col span="8">
+          <el-form-item label="创建时间">
+            <el-date-picker
+              startPlaceholder="开始时间"
+              endPlaceholder="结束时间"
+              type="daterange"
+            ></el-date-picker>
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-form>
+  </div>
 </template>
 
 <script lang="ts">
