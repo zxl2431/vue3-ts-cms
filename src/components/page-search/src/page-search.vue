9881e2e2326f295b99a1d176984e0053f8ec3dc3
zhangxianlin <zhangxianlin2431@163.com>
2021-12-26 00:04:42 +0800

    在form组件中增加两个插槽 header和footer 用户提示和搜索

--- src/components/page-search/src/page-search.vue
+++ src/components/page-search/src/page-search.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="page-search">
-    <hy-form v-bind="searchFormConfig"></hy-form>
+    <hy-form v-bind="searchFormConfig">
+      <template #header>
+        <h1 class="header">高级检索</h1>
+      </template>
+      <template #footer>
+        <div class="handle-btns">
+          <el-button>重置</el-button>
+          <el-button type="primary">搜索</el-button>
+        </div>
+      </template>
+    </hy-form>
   </div>
 </template>
 
@@ -24,4 +34,13 @@ export default defineComponent({
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.header {
+  color: red;
+  margin: 10px 0px;
+}
+.handle-btns {
+  text-align: right;
+  padding: 0 50px 20px 0;
+}
+</style>
