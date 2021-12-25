71fc35533e4f47ee346cc3ccdbeb970629f28699
zhangxianlin <zhangxianlin2431@163.com>
2021-12-25 17:10:18 +0800

    在nav-header的组件里面增加用户信息user-info.vue

--- src/components/nav-header/src/nav-header.vue
+++ src/components/nav-header/src/nav-header.vue
@@ -5,13 +5,22 @@
       :is="iconName"
       @click="handleFoldClick"
     ></component>
+    <div class="content">
+      <div>面包屑</div>
+      <user-info />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
+import UserInfo from './user-info.vue'
 
 export default defineComponent({
+  components: {
+    UserInfo
+  },
+
   emits: ['foldChange'],
 
   setup(props, { emit }) {
@@ -35,10 +44,21 @@ export default defineComponent({
 
 <style scoped lang="less">
 .nav-header {
+  display: flex;
+  width: 100%;
+
   .fold-meun {
     width: 30px;
     height: 30px;
     cursor: pointer;
+  }
+
+  .content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex: 1;
+    padding: 0 20px;
   }
 }
 </style>
