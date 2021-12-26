b3db80145050fd492ab8ae7feac5fb0c213b63e1
zhangxianlin <zhangxianlin2431@163.com>
2021-12-27 00:21:16 +0800

    在table中增加footer的分页功能

--- src/base-ui/table/src/table.vue
+++ src/base-ui/table/src/table.vue
@@ -42,7 +42,18 @@
 
     <!-- 搞一个脚 footer -->
     <div class="footer">
-      <slot name="footer"> </slot>
+      <slot name="footer">
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="currentPage4"
+          :page-sizes="[100, 200, 300, 400]"
+          :page-size="100"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="400"
+        >
+        </el-pagination>
+      </slot>
     </div>
   </div>
 </template>
