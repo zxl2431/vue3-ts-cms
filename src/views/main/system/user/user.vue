9544c71cebe78e458c548d5291c4ea2c06b233f8
zhangxianlin <zhangxianlin2431@163.com>
2021-12-26 12:36:04 +0800

    user组件中内容展示部分

--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="user">
     <page-search :searchFormConfig="searchFormConfig" />
+
+    <div class="content">
+      <el-table :data="userList" border style="width: 100%">
+        <el-table-column prop="name" label="用户名" minWidth="100">
+        </el-table-column>
+        <el-table-column prop="realname" label="真实姓名" minWidth="100" />
+        <el-table-column prop="cellphone" label="手机号码" minWidth="100" />
+        <el-table-column prop="createAt" label="创建时间" minWidth="250" />
+        <el-table-column prop="updateAt" label="更新时间" minWidth="250" />
+      </el-table>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
 import { useStore } from '@/store'
 
 import PageSearch from '@/components/page-search'
@@ -26,9 +37,20 @@ export default defineComponent({
       }
     })
 
-    return { searchFormConfig }
+    const userList = computed(() => store.state.system.userList)
+    const userCount = computed(() => store.state.system.userCount)
+
+    return {
+      searchFormConfig,
+      userList
+    }
   }
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.content {
+  padding: 20px;
+  border-top: 20px solid #f5f5f5;
+}
+</style>
