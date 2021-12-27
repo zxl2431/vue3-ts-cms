7cfb091899200ccb2101963461d73f5213e5efbb
zhangxianlin <zhangxianlin2431@163.com>
2021-12-28 00:01:16 +0800

    page-search组件中完成重置和搜索按钮的函数

--- src/components/page-search/src/page-search.vue
+++ src/components/page-search/src/page-search.vue
@@ -6,8 +6,8 @@
       </template>
       <template #footer>
         <div class="handle-btns">
-          <el-button>重置</el-button>
-          <el-button type="primary">搜索</el-button>
+          <el-button @click="handleResetClick">重置</el-button>
+          <el-button type="primary" @click="handleQueryClick">搜索</el-button>
         </div>
       </template>
     </hy-form>
@@ -28,8 +28,8 @@ export default defineComponent({
   components: {
     HyForm
   },
-
-  setup(props) {
+  emits: ['resetBtnClick', 'queryBtnClick'],
+  setup(props, { emit }) {
     // 双向绑定的属性应该是有配置文件的field来决定
     // 1.优化一: formData中的属性应该动态来决定
     const formItems = props.searchFormConfig?.formItems ?? []
@@ -38,11 +38,24 @@ export default defineComponent({
       formOriginData[item.field] = ''
     }
     const formData = ref(formOriginData)
+    // console.log('page-search组件中formData的初始值', formData)
 
-    console.log('page-search组件中formData的初始值', formData)
+    // 2.优化二: 当用户点击重置
+    const handleResetClick = () => {
+      formData.value = formOriginData
+      emit('resetBtnClick')
+    }
+
+    // 3.优化三: 当用户点击搜索
+    const handleQueryClick = () => {
+      // console.log('用户点击搜索')
+      emit('queryBtnClick')
+    }
 
     return {
-      formData
+      formData,
+      handleResetClick,
+      handleQueryClick
     }
   }
 })
