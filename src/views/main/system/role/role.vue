05dc2ce5c3b8dece01cd4b31f9dcaeb785919ee5
zhangxianlin <zhangxianlin2431@163.com>
2021-12-28 23:41:19 +0800

    完成role页面的search和content样子和初步数据

--- src/views/main/system/role/role.vue
+++ src/views/main/system/role/role.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="role">
-    <h2>role</h2>
+    <page-search :searchFormConfig="searchFormConfig"> </page-search>
+  </div>
+
+  <div class="content">
+    <page-content
+      ref="pageContentRef"
+      :contentTableConfig="contentTableConfig"
+      pageName="role"
+    >
+    </page-content>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+// search-form组件
+import PageSearch from '@/components/page-search'
+import { searchFormConfig } from './config/search.config'
+// content-table组件
+import PageContent from '@/components/page-content'
+import { contentTableConfig } from './config/content.config'
 
 export default defineComponent({
   name: 'role',
+  components: {
+    PageSearch,
+    PageContent
+  },
   setup() {
-    return {}
+    return {
+      searchFormConfig,
+      contentTableConfig
+    }
   }
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.content {
+  padding: 20px;
+  border-top: 20px solid #f5f5f5;
+}
+</style>
