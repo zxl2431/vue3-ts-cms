99a49d478adff1a34441c69d94e6c923f30d5fe3
zhangxianlin <zhangxianlin2431@163.com>
2021-12-28 23:16:25 +0800

    修改page-content组件,解决分页的bug currentPage -1, 而且默认的currentPage改为1

--- src/components/page-content/src/page-content.vue
+++ src/components/page-content/src/page-content.vue
@@ -64,15 +64,17 @@ export default defineComponent({
     const store = useStore()
 
     // 1.双向绑定
-    const pageInfo = ref({ currentPage: 0, pageSize: 10 })
+    const pageInfo = ref({ currentPage: 1, pageSize: 10 })
     watch(pageInfo, () => getPageData())
 
     // 发送网络请求
     const getPageData = (queryInfo: any = {}) => {
+      // console.log('page-content组件', pageInfo)
+
       store.dispatch('system/getPageListAction', {
         pageName: props.pageName,
         queryInfo: {
-          offset: pageInfo.value.currentPage * pageInfo.value.pageSize,
+          offset: (pageInfo.value.currentPage - 1) * pageInfo.value.pageSize,
           size: pageInfo.value.pageSize,
           ...queryInfo
         }
